b565c1ea58684858e0a829dfdbff6457533fca10
Nicolas <nicolas.m.o@eaportal.org>
2023-10-26 16:42:43 -0300

    site concertado finalmente

--- src/pages/Bibliografia.jsx
+++ src/pages/Bibliografia.jsx
@@ -13,15 +13,15 @@ export default function Bibliografia() {
             <section className="container">
                 <div className="box">
                     <h1>Origem</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis placeat reiciendis maxime. Ut dolores ipsam labore delectus quidem corrupti eius voluptate voluptates, provident non beatae rem cupiditate, eum dignissimos reiciendis.</p>
+                    <p>O projeto FireWarden foi desenvolvido com o propósito de prevenir, detectar e proteger, uma vez que muitas vidas são perdidas devido a incêndios, e muitas pessoas acabam perdendo suas casas.</p>
                 </div>
                 <div className="box">
                     <h1>Artigo</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis placeat reiciendis maxime. Ut dolores ipsam labore delectus quidem corrupti eius voluptate voluptates, provident non beatae rem cupiditate, eum dignissimos reiciendis.</p>
+                    <p>Nós pesquisamos artigos para começar a nossa ideia e adquirir um melhor entendimento sobre o projeto, explorando o funcionamento de um sistema de alarme de incêndio e seus componentes.</p>
                 </div>
                 <div className="box">
                     <h1>Referências</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis placeat reiciendis maxime. Ut dolores ipsam labore delectus quidem corrupti eius voluptate voluptates, provident non beatae rem cupiditate, eum dignissimos reiciendis.</p>
+                    <p>Para obter informações, utilizamos alguns vídeos que explicavam o funcionamento do Arduino, as etapas para fazer o projeto funcionar de maneira ideal e como utilizar o aplicativo relacionado.</p>
                 </div>
             </section>
 
